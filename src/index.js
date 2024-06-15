09ef9c0b959f31e4b54d4773a1015727e0ab87c4
As7Vezes <denysn60@gmail.com>
2024-06-15 16:36:10 -0300

    feat: add statement retrivial endpoint

--- src/index.js
+++ src/index.js
@@ -5,27 +5,39 @@ const app = express();
 
 app.use(express.json());
 
-const constumers = [];
+const custumers = [];
 
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
 
-    const cpfExist = constumers.some(constumer => constumer.cpf === cpf)
+    const cpfExist = custumers.some(constumer => constumer.cpf === cpf)
 
     if(cpfExist) {
         res.status(400).json({ error: "Customer already Exists!" })
     }
 
-    constumers.push({
+    custumers.push({
         cpf,
         name,
         id: uuidv4(),
         statement: []
     });
 
-    console.log(constumers);
+    console.log(custumers);
     return res.status(201).send();
 });
+
+app.get("/statement", (req, res) => {
+    const { cpf } = req.headers;
+
+    const customer = custumers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return res.status(400).json({ error: "Custumer not found!" })
+    }
+
+    return res.json(customer.statement)
+})
 
 const port = 3000;
 
